e47626d10bab23f95eef3d696827fb73ee59d073
sitevk <ekateryna123@fex.net>
2017-12-14 16:31:52 +0200

    commit-12

--- Js/form.js
+++ Js/form.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
             var form_data = $(this).serialize(); //собераем все данные из формы
             $.ajax({
             type: "POST", //Метод отправки
-            url: "mail.php", //путь до php фаила отправителя
+            url: "../mail.php", //путь до php фаила отправителя
             data: form_data,
             success: function() {
                    //код в этом блоке выполняется при успешной отправке сообщения
